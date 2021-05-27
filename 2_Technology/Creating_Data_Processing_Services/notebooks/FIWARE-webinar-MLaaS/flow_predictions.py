a291b5f76eb8fb077c28800f82a946c8b41aaf80
Dennis Wendland <dennis.wendland@fiware.org>
2021-05-27 08:41:47 +0200

    Material and notebooks for MLaaS webinar

---
+++ 2_Technology/Creating_Data_Processing_Services/notebooks/FIWARE-webinar-MLaaS/flow_predictions.py
@@ -0,0 +1,20 @@
+from bentoml import env, artifacts, api, BentoService
+from bentoml.adapters import JsonInput, JsonOutput
+from bentoml.frameworks.sklearn import SklearnModelArtifact
+
+
+# @env(infer_pip_packages=True)
+@env(
+    pip_packages=[
+        'scikit-learn==0.24.2',
+        'numpy==1.20.2',
+        'pytz==2021.1'
+    ]
+)
+@artifacts([SklearnModelArtifact('flow_model')])
+class TestFlowModel(BentoService):
+    @api(input=JsonInput(), output=JsonOutput())
+    def predict(self, rainfall):
+        # transform the input as a numpy array
+        flow = self.artifacts.flow_model.predict(rainfall)
+        return(flow)
---
+++ 2_Technology/Creating_Data_Processing_Services/notebooks/FIWARE-webinar-MLaaS/package_flow_model.py
@@ -0,0 +1,17 @@
+import pickle
+from flow_predictions import TestFlowModel
+
+# Load the previously saved sklearn model
+with open('./linear_model.pickle', 'rb') as f:
+    model = pickle.load(f)
+
+# Create an instance of the FlowModel service
+flow_predictions_service = TestFlowModel()
+
+# Pack the trained model
+flow_predictions_service.pack('flow_model', model)
+
+# Save the prediction service to disk for model serving
+saved_path = flow_predictions_service.save()
+
+print('done')
